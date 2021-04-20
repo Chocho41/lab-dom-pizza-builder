fe6f811b2735967c7ca0e945fb9ded35e6dc84d8
Quentin Chauvat <quentin.chauvat@gmail.com>
2021-04-20 17:03:40 +0200

    last updates from tuesday morning

--- js/index.js
+++ js/index.js
@@ -43,27 +43,152 @@ function renderPepperoni() {
 }
 
 function renderMushrooms() {
-  // Iteration 1: set the visibility of `<section class="mushroom">`
+  // Iteration 1: set the visibility of `<section class="mushroom">`*
+  document.querySelectorAll('.mushroom').forEach(oneMushroom => {
+    if (state.mushrooms) {
+      oneMushroom.style.visibility = 'visible';
+    } else {
+      oneMushroom.style.visibility = 'hidden';
+    }
+  });
 }
 
 function renderGreenPeppers() {
   // Iteration 1: set the visibility of `<section class="green-pepper">`
+  document.querySelectorAll('.green-pepper').forEach(oneGreenPeppers => {
+    if (state.greenPeppers) {
+      oneGreenPeppers.style.visibility = 'visible';
+    } else {
+      oneGreenPeppers.style.visibility = 'hidden';
+    }
+  });
 }
 
 function renderWhiteSauce() {
   // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
-}
+  document.querySelectorAll('.sauce-white').forEach(oneSauceWhite => {
+    const whiteSauce = document.querySelector(".sauce")
+      console.log(whiteSauce);
+      whiteSauce.classList.toggle("sauce-white")
+    });
+  }
 
 function renderGlutenFreeCrust() {
   // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
+  document.querySelectorAll('.crust-gluten-free').forEach(oneGlutenFreeCrust=> {
+    const crust1 = document.querySelector(".crust")
+      console.log(crust1);
+      crust1.classList.toggle("crust-gluten-free")
+  }); 
 }
 
-function renderButtons() {
+function renderButtons(evt) {
   // Iteration 3: add/remove the class "active" of each `<button class="btn">`
+ //console.log(evt.target);
+  //const clickedButton = evt.target;
+  //clickedButton.classList.toggle("is-active");
+
+const active1 = document.querySelectorAll(".btn");
+console.log(active1);
+if(state.pepperoni === true) {
+  document.querySelector(".btn-pepperoni").classList.add("active");
+  console.log(btnPep);
+} else{
+  document.querySelector(".btn-pepperoni".classList.remove("active"));
+};
+if(state.mushrooms === true) {
+  document.querySelector(".btn-mushrooms").classList.add("active");
+  console.log(btnPep);
+} else{
+  document.querySelector(".btn-mushrooms".classList.remove("active"));
+};
+if(state.greenPeppers === true) {
+  document.querySelector(".btn-green-peppers").classList.add("active");
+  console.log(btnPep);
+} else{
+  document.querySelector(".btn-green-peppers".classList.remove("active"));
+};
+if(state.whiteSauce === true) {
+  document.querySelector(".btn-sauce").classList.add("active");
+  console.log(btnPep);
+} else{
+  document.querySelector(".btn-sauce".classList.remove("active"));
+};
+if(state.glutenFreeCrust === true) {
+  document.querySelector(".btn-crust").classList.add("active");
+  console.log(btnPep);
+} else{
+  document.querySelector(".btn-crust".classList.remove("active"));
+};
+
 }
 
-function renderPrice() {
-  // Iteration 4: change the HTML of `<aside class="panel price">`
+
+
+function renderPrice(prices) {
+// Iteration 4: change the HTML of `<aside class="panel price">`
+ingredients.foreach((ingredient) => {
+  const list = document.createElement("ul");
+  list.className = "price-ingredient";
+  
+  const markupPepperoni = 
+    `<li> $1 ${ingredient.name}</li>`;
+  const markupMushroom = 
+  `<li> $1 pepperoni</li>`;
+  const markupGreenPeppers = 
+    `<li> $1 pepperoni</li>`;
+  const markupWhiteSauce = 
+    `<li> $3 pepperoni</li>`;
+  const markupGlutenFreeCrust = 
+    `<li> $5 pepperoni</li>`;
+  
+  const pepperoniPrice = document.getElementById("pepperoni-price");
+  const mushroomsPrice = document.getElementById("mushroom-price");
+  const greenPeppersPrice = document.getElementById("green-pepper-price");
+  const whiteSaucePrice = document.getElementById("white-sauce-price");
+  const glutenFreeCurstPrice = document.getElementById("gluten-free-crust-price");
+
+  let totalPrice = 10;
+  
+  if(state.pepperoni === true) {
+    list.innerHTML = markupPepperoni;
+    listPrice.appendChild(card);
+    totalPrice =+ 1;
+  } else {
+    pepperoniPrice.remove();
+  };
+  if(state.mushrooms === true) {
+    priceIngredient.innerHTML = markupMushroom;
+    listPrice.appendChild(card);
+    totalPrice =+ 1;
+  } else {
+    mushroomsPrice.remove();
+  };
+  if(state.greenPeppers === true) {
+    priceIngredient.innerHTML = markupGreenPeppers;
+   listPrice.appendChild(card);
+   totalPrice =+ 1;
+  } else {
+    greenPeppersPrice.remove();
+  };
+  if(state.whiteSauce === true) {
+  priceIngredient.innerHTML = markupWhiteSauce;
+  listPrice.appendChild(card);
+  totalPrice =+ 3;
+} else {
+  whiteSaucePrice.remove();
+};
+  if(state.glutenFreeCrust === true) {
+  priceIngredient.innerHTML = markupGlutenFreeCrust;
+  listPrice.appendChild(card);
+  totalPrice =+ 5;
+} else {
+  glutenFreeCurstPrice.remove();
+};
+});
+
+totalPriceMarkup = `${totalPrice}`;
+
 }
 
 renderEverything();
@@ -75,9 +200,25 @@ document.querySelector('.btn.btn-pepperoni').addEventListener('click', () => {
 });
 
 // Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
+document.querySelector('.btn.btn-mushrooms').addEventListener('click', () => {
+  state.mushrooms = !state.mushrooms;
+  renderEverything();
+});
 
 // Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
+document.querySelector('.btn.btn-green-peppers').addEventListener('click', () => {
+  state.greenPeppers = !state.greenPeppers;
+  renderEverything();
+});
 
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
+document.querySelector('.btn.btn-sauce').addEventListener('click', () => {
+  state.whiteSauce = !state.whiteSauce;
+  renderEverything();
+});
 
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+document.querySelector('.btn.btn-crust').addEventListener('click', () => {
+  state.glutenFreeCrust = !state.glutenFreeCrust;
+  renderEverything();
+});
